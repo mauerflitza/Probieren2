375c316d50947ef126640db964819a72033142e8
mauerflitza <tobias.friedrich1@gmx.de>
2016-07-15 02:30:15 +0000

    Verbesserung der Empfangsfunktion

--- pythonCAN_empfangen_printer.py
+++ pythonCAN_empfangen_printer.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
+
+import os
 import time
 import can 
+from threading import Timer
 
 can_channel = 'vcan0'
 
 class myPrinter(can.Listener):
-	def _init_(self,open_file):
+	def __init__(self,open_file):
 		print("Printer wurde erstellt")
 		self.logfile=open_file
    
 	def on_message_received(self, msg):
 		self.logfile.write(str(msg))
+		self.logfile.write("\n")
 		print(msg)
 
-	def _del_(self):
+	def __del__(self):
+		self.logfile.close()
 		print("Prozess abgebrochen")
-		
-logfile=open('logfile', 'w')   
+
+def ende():
+	bus.shutdown()
+	logfile.close()
+	print("Ende")
+	time.sleep(1)
+	print("Schliessen")
+
+logfile_path = os.path.join('/home/pi/datalogger/virtual/myrepo/Probieren2/logfiles', time.strftime('%Y%m%d-%H%M'))
+logfile = open(logfile_path, 'w')   
+
 bus = can.interface.Bus(can_channel, bustype="socketcan_native")
 notifier = can.Notifier(bus, [myPrinter(logfile)])
 
+starttime= time.time()
 try:
-   while True:
+   while time.time()<(starttime+10):
       time.sleep(0.1)
+   ende()
 except KeyboardInterrupt:
-   bus.shutdown()
-   logfile.close()
-   print("Ende")
-   time.sleep(1)
-   print("Schliessen")
+   ende()
