292554e53ef36a23a619d6ece018f623e321e3f0
mauerflitza <tobias.friedrich1@gmx.de>
2016-07-15 15:59:09 +0900

    Create Listener-Printer-Thread.py

---
+++ Listener-Printer-Thread.py
@@ -0,0 +1,46 @@
+import threading
+import queue	
+import can
+import time
+
+q_logs = queue.Queue()
+
+class Listener(threading.Thread):
+	def __init__(self, end_flag):
+		threading.Thread.__init__(self)
+		self.ende=end_flag
+		self.bus = can.interface.Bus("vcan0", bustype="socketcan_native")
+	def run(self): 
+		while not self.ende.isSet():
+			mesg=self.bus.recv()
+			print(mesg)
+			q_logs.put(mesg)
+
+
+class Printer(threading.Thread):
+	def __init__(self,logfile, end_flag):
+		threading.Thread.__init__(self)
+		self.ende=end_flag
+		self.logfile = logfile
+	def run(self): 
+		while not self.ende.isSet():
+			if not q_logs.empty():
+				mesg=q_logs.get()
+			print(mesg)
+			self.logfile.write(mesg)
+			self.logfile.write("\n")
+			
+if __name__ == '__main__':
+	end_Flag = threading.Event()
+	logs = open('test', 'w')
+	
+	Listen_Thread = Listener(end_Flag)
+	Print_Thread = Printer(end_Flag)
+	
+	Listen_Thread.start()
+	Print_Thread.start()
+	
+	time.sleep(5)
+	end_Flag.set()	
+	Print_Thread.join()
+	self.logfile.close()
