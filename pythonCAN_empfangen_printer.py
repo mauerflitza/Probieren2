7dc21e7a1ee01700a091ccc98b4ef828f1a3ff6a
mauerflitza <tobias.friedrich1@gmx.de>
2016-07-15 11:06:22 +0900

    Update pythonCAN_empfangen_printer.py

--- pythonCAN_empfangen_printer.py
+++ pythonCAN_empfangen_printer.py
@@ -5,20 +5,21 @@ import can
 can_channel = 'vcan0'
 
 class myPrinter(can.Listener):
-
-   def _init_(self,open_file):
-      print("Printer wurde erstellt")
-      self.logfile=open_file
+	def _init_(self,open_file):
+		print("Printer wurde erstellt")
+		self.logfile=open_file
    
-   def on_message_received(self, msg):
-      self.logfile.write(str(msg))
-      print(msg)
+	def on_message_received(self, msg):
+		self.logfile.write(str(msg))
+		print(msg)
 
-   def _del_(self):
-      print("Prozess abgebrochen")
+	def _del_(self):
+		print("Prozess abgebrochen")
+		
 logfile=open('logfile', 'w')   
 bus = can.interface.Bus(can_channel, bustype="socketcan_native")
-notifier = can.Notifier(bus, [myPrinter()])
+notifier = can.Notifier(bus, [myPrinter(logfile)])
+
 try:
    while True:
       time.sleep(0.1)
@@ -28,4 +29,3 @@ except KeyboardInterrupt:
    print("Ende")
    time.sleep(1)
    print("Schliessen")
-  
