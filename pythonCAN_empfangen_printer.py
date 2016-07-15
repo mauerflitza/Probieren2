4ed2e53133a345305c8babeb1f847f7dc4807834
mauerflitza <tobias.friedrich1@gmx.de>
2016-07-15 02:01:59 +0000

    Add existing file

---
+++ pythonCAN_empfangen_printer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import time
+import can 
+
+can_channel = 'vcan0'
+
+class myPrinter(can.Listener):
+
+   def _init_(self,open_file):
+      print("Printer wurde erstellt")
+      self.logfile=open_file
+   
+   def on_message_received(self, msg):
+      self.logfile.write(str(msg))
+      print(msg)
+
+   def _del_(self):
+      print("Prozess abgebrochen")
+logfile=open('logfile', 'w')   
+bus = can.interface.Bus(can_channel, bustype="socketcan_native")
+notifier = can.Notifier(bus, [myPrinter()])
+try:
+   while True:
+      time.sleep(0.1)
+except KeyboardInterrupt:
+   bus.shutdown()
+   logfile.close()
+   print("Ende")
+   time.sleep(1)
+   print("Schliessen")
+  
